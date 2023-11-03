abe47f3ead211d23a5a73f468ada157d2580c416
VMware Code <noreply@vmware.com>
2023-11-03 14:30:41 +0000

    Initial commit by Sonny Nguyen

--- snippet.ps1
+++ snippet.ps1
@@ -1,17 +1,13 @@
+"set-executionpolicy unrestricted"
+"install-module SkylineAutomationToolkit"
+
+
 "skyline-comm get-findings list|longlistt send2slack|send2servicenow|send2jira|send2csv"
-b
 "skyline-comm get-findings detail|longdetail FINDINGID SOURCE send2slack|send2servicenow|send2jira|send2csv"
-c
 "skyline-comm get-findings top 5|10|25|50|200 send2slack|send2servicenow|send2jira|send2csv"
-d
 "skyline-comm get-findings custom send2slack|send2servicenow|send2jira|send2csv"
-e
 "skyline-comm get-findings detailall FILE.CSV"
-f
 "skyline-comm get-findings category SECURITY|NETWORK|COMPUTE|STORAGE send2slack|send2servicenow|send2jira|send2csv"
-g
 "skyline-comm get-findings type CONFIGURATION|UPGRADE send2slack|send2servicenow|send2jira|send2csv"
-h
 "skylline-comm get-findings severity CRITICAL|MODERATE|TRIVIAL send2slack|send2servicenow|send2jira|send2csv"
-i
 "skylline-comm email SMTP-SERVER FROM-EMAIL TO-EMAIL FILE.CSV"
