643531637af7bf37f7f5266e4cc4e68d8bbeab0b
VMware Code <noreply@vmware.com>
2023-11-09 18:24:12 +0000

    Initial commit by Sonny Nguyen

--- snippet.ps1
+++ snippet.ps1
@@ -2,8 +2,10 @@
 "install-module SkylineAutomationToolkit"
 
 REM: user guide is in c:\program files\windowspowershell\module\SkylineAutomationToolkit\1.x.x\Skyline Automation Toolkit 1.x.x.pdf"
+REM: there is a readme.txt in that folder as well. I will update info with changes.
 
-"skyline-comm get-findings list|longlistt send2slack|send2servicenow|send2jira|send2csv"
+
+"skyline-comm get-findings list|listmore|longlistt send2slack|send2servicenow|send2jira|send2csv"
 "skyline-comm get-findings detail|longdetail FINDINGID SOURCE send2slack|send2servicenow|send2jira|send2csv"
 "skyline-comm get-findings top 5|10|25|50|200 send2slack|send2servicenow|send2jira|send2csv"
 "skyline-comm get-findings custom send2slack|send2servicenow|send2jira|send2csv"
